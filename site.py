9453636915fd0a107504e9ea787139aa39e2fbd3
OneDog <nazarkondrateko121@gmail.com>
2024-10-11 13:16:06 +0300

    test

---
+++ site.py
@@ -0,0 +1,13 @@
+import flask
+
+
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return "<h1>Hello World</h1>"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
